985522d19c62d07f9517b719a087bbd37860bc05
JonadanKim <jw11423@gmail.com>
2022-04-30 00:43:12 +0900

    2022-04-29

--- selfdev/dstep36_1.py
+++ selfdev/dstep36_1.py
@@ -3,7 +3,7 @@
 
 if '__file__' in globals():
     import os, sys
-    sys.path.append(os.path.join(os.path.dirname(__file__), '/home/jwkim/dev/venv03/selfpkg'))
+    sys.path.append(os.path.join(os.path.dirname(__file__), '../selfpkg'))
 
 import numpy as np
 from dezero import Variable
--- selfdev/dstep38.py
+++ selfdev/dstep38.py
@@ -1,6 +1,6 @@
 if '__file__' in globals():
     import os, sys
-    sys.path.append(os.path.join(os.path.dirname(__file__), '/home/jwkim/dev/venv03/selfpkg'))
+    sys.path.append(os.path.join(os.path.dirname(__file__), '../selfpkg'))
 
 import numpy as np
 
